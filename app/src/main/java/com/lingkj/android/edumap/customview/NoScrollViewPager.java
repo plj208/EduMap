a88d7d5ead818498be294891f2706a625c429167
panlijun <849226133@qq.com>
2018-05-11 14:37:14 +0800

    some content commit

---
+++ app/src/main/java/com/lingkj/android/edumap/customview/NoScrollViewPager.java
@@ -0,0 +1,32 @@
+package com.lingkj.android.edumap.customview;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * author: panlijun
+ * time: 2018/5/11 上午10:39
+ * detail:
+ */
+public class NoScrollViewPager extends ViewPager {
+    public NoScrollViewPager(Context context) {
+        super(context);
+    }
+
+    public NoScrollViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+}
